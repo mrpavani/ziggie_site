c98c885a87bbcdc268f18b0997f6dcfcd4f5dcfa
Marcos Pavani <marcospavani@gmail.com>
2022-06-20 11:06:11 -0300

    Sites da Ziggie

---
+++ site_b2c/js/modal.js
@@ -0,0 +1,19 @@
+const switchModal = () => {
+    const modal = document.querySelector('.modal')
+    const actualStyle = modal.style.display
+    if(actualStyle == 'block'){
+        modal.style.display = 'none';
+    }else{
+        modal.style.display = 'block';
+    }
+}
+
+const btn = document.querySelector('.top__menu')
+btn.addEventListener('click', switchModal)
+
+window.onclick = function(event) {
+    const modal = document.querySelector('.modal')
+    if (event.target== modal) {
+        switchModal()
+    }
+}
